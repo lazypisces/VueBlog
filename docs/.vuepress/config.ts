72ad38d34f8d86ac0d56738307c689854030de6f
懶魚 <pickupcamry@gmail.com>
2022-04-04 00:17:01 +0800

    v0 初始創建，測試作者

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,113 @@
+import { defineUserConfig } from 'vuepress'
+import type { DefaultThemeOptions } from 'vuepress'
+
+export default defineUserConfig<DefaultThemeOptions>({
+    // 站点配置
+    // lang: 'en-US',
+    lang: 'zh-TW',
+    title: '全自動股票管理表',
+    description: 'Just playing around',
+    base: '/MyBlog/',
+
+    // 主题和它的配置
+    theme: '@vuepress/theme-default',
+    themeConfig: {
+        logo: 'https://vuejs.org/images/logo.png',
+        contributorsText:"作者",
+        lastUpdatedText:"最後更新",
+        // navbar: [
+        //     // NavbarItem
+        //     {
+        //         text: 'Foo',
+        //         link: '/foo/',
+        //     },
+        //     // NavbarGroup
+        //     {
+        //         text: 'Group',
+        //         children: ['/group/foo.md', '/group/bar.md'],
+        //     },
+        //     // 字符串 - 頁面文件路徑
+        //     '/bar/README.md',
+        // ],
+        //導航欄
+        navbar: [
+            { text: 'Home', link: '/' },  //text:導航標題內容
+            {
+                text: '指南',
+                children: [
+                    { text: '快速上手', link: '/StockProfolioDocs/QuickStart/介紹.md', activeMatch: '^/StockProfolioDocs/QuickStart/',  },
+                    { text: '基本功能', link: '/StockProfolioDocs/Introduction/儀表板.md', activeMatch: '^/StockProfolioDocs/Introduction/',  },
+                    { text: '進階功能', link: '/StockProfolioDocs/PayOnly/進階功能簡介.md', activeMatch: '^/StockProfolioDocs/PayOnly/', },
+                ]
+            },
+            {
+                text: '版本',
+                children: [
+                    { text: '台股免費版', link: '/StockProfolioDocs/' },
+                    { text: '台股付費版', link: '/StockProfolioDocs/' },
+                    { text: '美股付費版', link: '/StockProfolioDocs/' },
+                    { text: '客製化(含範例)', link: '/StockProfolioDocs/' },
+                ]
+            },
+            {
+                text: 'Contact',
+                children: [
+                    { text: '懶魚', link: '/' },
+                    { text: 'Markdown語法', link: '/StockProfolioDocs/Introduction/Introduction0.md'},
+                ]
+            },
+            { text: '贊助', link: 'https://payment.ecpay.com.tw/Broadcaster/Donate/C014318DF58EE9F80D220D97FB4FE5C8' },
+        ],
+        // sidebar: 'auto',
+        sidebar: {
+            '/StockProfolioDocs/': [
+                // '/StockProfolioDocs/',
+                // '/StockProfolioDocs/Introduction/',
+                {
+                    text: '快速上手',
+                    collapsible: true,
+                    children: [
+                        '/StockProfolioDocs/QuickStart/介紹.md',
+                        '/StockProfolioDocs/QuickStart/快速上手.md',
+                    ]
+                },
+                {
+                    text: '基本功能',
+                    collapsible: true,
+                    children: [
+                        '/StockProfolioDocs/Introduction/儀表板.md',
+                        '/StockProfolioDocs/Introduction/交易紀錄.md',
+                        '/StockProfolioDocs/Introduction/持股&歷史損益.md',
+                        '/StockProfolioDocs/Introduction/券商與分類.md',
+                        '/StockProfolioDocs/Introduction/股票代號.md',
+                    ]
+                },
+                {
+                    text: '⭐進階功能',
+                    collapsible: true,
+                    // link: '/StockProfolioDocs/PayOnly/test1.md',
+                    children: [
+                        '/StockProfolioDocs/PayOnly/進階簡介.md',
+                        '/StockProfolioDocs/PayOnly/購買.md',
+                        '/StockProfolioDocs/PayOnly/功能.md',
+                    ]
+                },
+            ],
+        }
+    },
+    plugins: [
+        [
+            '@vuepress/plugin-search',
+            {
+                locales: {
+                    '/': {
+                        placeholder: 'Search',
+                    },
+                    '/zh/': {
+                        placeholder: '搜索',
+                    },
+                },
+            },
+        ],
+    ],
+})
---
+++ docs/README.md
@@ -0,0 +1,62 @@
+---
+lang: zh-TW
+title: 頁面的標題
+description: 頁面的描述
+home: true
+# heroImage: /images/台股付費版-儀表板.jpg
+heroText: Vue 標題
+tagline: Vue 副標題
+actionText: 快速上手 →
+actionLink: /StockProfolioDocs/
+
+actions:
+  - text: 快速上手 →
+    link: /StockProfolioDocs/QuickStart/快速上手.md
+    type: primary
+  - text: 範本預覽
+    link: /StockProfolioDocs/QuickStart/快速上手.md#各版本預覽
+    type: secondary
+
+features:
+- title: 自動更新股票成交價
+  details: 開啟後自動抓取最新成交價(非即時)，以利後續各種損益計算。
+- title: 多種視覺化圖表
+  details: 享受一切數據自動計算並與圖表連動，一開啟便自動更新圖表數據。
+- title: 自動計算損益&報酬率
+  details: 依據最新抓取的各檔股票成交價，計算 已/未實現的損益和報酬率，並將其圖表化。
+- title: 一鍵更新
+  details: 每次有新的交易要做紀錄時，提供一個按鈕。點按後更新所有圖表和計算的功能。
+- title: 輕鬆管理多家券商
+  details: 靈活的設計使的本表可以同時管理不同的券商並設定不同的折讓手續費，也能看各券商/分類的占比圖。
+- title: 自動記錄市值變化
+  details: 每周五盤後，會將當下的成本、市值、未實現損益等記錄下來。當時間拉長，可以觀察到自己投資的曲線變化，是付費版的其中一個大功能！
+
+footer: MIT Licensed | Copyright © 2018-present Evan You
+---
+
+### 只要3步驟，讓管理股票像數 1, 2, 3 一樣容易
+
+#### 1. 點選連結 [`建立副本用`](https://docs.google.com/spreadsheets/d/1rchpA3W-BlDn8BZOhQ1uiUPnuHLb4UZ65iLzjHb-cp0/copy) → 點選按鈕 `建立副本`
+   ::: tip 收費版： 在確認收款後將會透過Google寄出系統email，信件內含可建立收費版的連結
+   :::
+
+#### 2. 給自己的管理表一個新的名子
+   ::: tip 將「全自動股票管理表 | 免費版 - 副本」← 改名
+   :::
+
+#### 3. 啟用一鍵更新 ( 含自動取價 )
+   - 在試算表內點選【儀表版】分頁的按鈕 <Badge text="更新交易紀錄" vertical="middle"/> → 點選 __「進階」__ → 點選  __「前往取得股票現價(不安全)」__ → 點選按鈕 __`允許`__
+   - 允許後再次點選【儀表版】分頁的按鈕 <Badge text="更新交易紀錄" vertical="middle"/>，確認功能正常
+   
+   ::: details 啟用示意圖
+   ![儀表板按鈕](/images/更新交易紀錄按鈕.jpg)
+   ![授權步驟1](/images/授權步驟1.jpg)
+   ![授權步驟2](/images/授權步驟2.jpg)
+   ![授權步驟3](/images/授權步驟3.jpg)
+   :::
+
+   ::: warning 💡 如有安全上的疑慮，請勿使用此範本。因為不授權基本上這份範本主要功能都不能使用
+   ps. 所有非Google的原生功能(額外添加的)，Google一律會跳出此警告
+   :::
+
+#### 開始使用 
